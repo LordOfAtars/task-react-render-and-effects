10cfb11d09b24c6e4bbc64c6a4bc0231d3691798
Art Smirnov <temismirniy@gmail.com>
2025-05-19 20:29:48 +0300

    task

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,30 @@
+import React, { useEffect, useState } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
-export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+interface Props {
+    sourceId: string;
+}
+
+export function Effects({ sourceId }: Props) {
+    const [lastMessage, setLastMessage] = useState(-1);
+
+    const handleNewMessage = (message: number | null) => {
+        if (message !== null && message !== undefined) {
+            setLastMessage(message);
+        }
+    };
+
+    useEffect(() => {
+        const currentSourceId = sourceId;
+
+        subscribe(currentSourceId, handleNewMessage);
+
+        setLastMessage(-1);
+
+        return () => {
+            unsubscribe(currentSourceId, handleNewMessage);
+        };
+    }, [sourceId]);
+
+    return <div>{`${sourceId}: ${lastMessage}`}</div>;
 }
